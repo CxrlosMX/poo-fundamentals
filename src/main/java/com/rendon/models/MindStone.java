96fc359f8335373f93368c953f307d37142430a6
CxrlosMX <LuisCRendon131@gmail.com>
2025-06-15 16:25:52 -0600

    prototype

--- src/main/java/com/rendon/models/MindStone.java
+++ src/main/java/com/rendon/models/MindStone.java
@@ -1,7 +1,11 @@
 package com.rendon.models;
 
 import lombok.ToString;
+import lombok.extern.java.Log;
 
+import java.io.*;
+
+@Log
 @ToString
 public class MindStone extends Stone{
 
@@ -21,4 +25,30 @@ public class MindStone extends Stone{
     public void usePower() {
         System.out.println("Using Mind Stone: " + super.toString());
     }
+
+    public MindStone getPrototype(){
+        try (
+                /*convert object into bytes*/
+                final var bos= new ByteArrayOutputStream();
+                final var outs= new ObjectOutputStream(bos);){
+
+            /*serialize the object: process of converting an object's state into a byte stream: JSON, XML, Bytes*/
+            outs.writeObject(this);
+            outs.flush();
+
+            try(
+                    /*deserialization of the objet: converting a byte stream back into a Java object*/
+                    final var bis= new ByteArrayInputStream(bos.toByteArray());
+                    final var ois= new ObjectInputStream(bis);
+                    ) {
+                /*return and cast*/
+                return (MindStone) ois.readObject();
+            }
+
+        }catch (IOException | ClassNotFoundException e){
+            log.warning("can´t getPrototype");
+            throw new RuntimeException();
+    }
+    }
+
 }
